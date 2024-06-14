855087db38ae974f799ec787b13b2f009ebcc975
Trible69 <edwinamunoz357@gmail.com>
2024-06-14 13:11:46 -0500

    getRainbow function

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ using namespace sf;
 
 static Color getRainbow(float t){
     const float r = sin(t);
-    const float g = sin(t);
-    const float b = sin(t);
+    const float g = sin(t+0.33f*2.0f*Math::pi);
+    const float b = sin(t+0.66f*2.0f*Math::pi);
+    return {static_cast<uint8_t>(255.0f*r*r), static_cast<uint8_t>(255.0f*g*g), static_cast<uint8_t>(255.0f*b*b)};
+
 };
