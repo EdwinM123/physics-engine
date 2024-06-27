2b9d47df1e68d1a46c6304a6a2388223cdfb0f5c
Trible69 <edwinamunoz357@gmail.com>
2024-06-27 10:53:09 -0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <SFML/Window/ContextSettings.hpp>
 
 using namespace std;  
+using namespace sf;
 
 static sf::Color getRainbow(float t){
     const float r = sin(t);
@@ -20,5 +21,43 @@ static sf::Color getRainbow(float t){
 int32_t main(int32_t, char*[]){
     constexpr int32_t window_width = 1000;
     constexpr int32_t window_height =1000;
+    
+    sf::ContextSettings settings; 
+    settings.antialiasingLevel =1; 
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet", sf::Style::Default, settings);
+    const uint32_t frame_rate =60; 
+    window.setFramerateLimit(frame_rate);
 
+    Solver solver; 
+    Renderer renderer{window};
+
+    solver.setConstraint({static_cast<float>(window_width) *0.5f, static_cast<float>(window_height)*0.5f}, 450.0f);
+    solver.setSubStepsCount(8);
+    solver.setSimulationUpdateRate(frame_rate);
+
+    //Simulation Attributes
+    const float         object_spawn_delay  = 0.025f;
+    const float         object_spawn_speed  = 1200.0f;
+    const sf::Vector2f  object_spawn_position={500.0f, 200.0f};
+    const float         object_min_radius   = 1.0f; 
+    const float         object_max_radius   = 20.0f; 
+    const uint32_t      max_objects_count   = 1000; 
+    const float         max_angle           = 1.0f; 
+
+    sf::Clock clock;
+
+    //Main Loop 
+    while (window.isOpen()) {
+      sf::Event event{};
+      while(window.pollEvent(event)){
+        if(event.type==sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+          window.close();
+        }
+      }
+    }
+
+    solver.update();
+    window.clear(sf::Color::White);
+    renderer.render(solver);
+    window.display();
 }
