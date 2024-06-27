7f2f0324f36db6514dba30a1b41f360dcbd3b023
Trible69 <edwinamunoz357@gmail.com>
2024-06-27 11:06:51 -0500

    renderer

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -1,1 +1,36 @@
 #pragma once
+#include "solver.hpp"
+
+class Renderer {
+  public: 
+    explicit 
+      Renderer(sf::RenderTarget& target)
+        : m_target{target} {
+
+        }
+
+      void render(const Solver& solver) const{
+        //Constraint 
+        const sf::Vector3f constraint = solver.getConstraint(); 
+        sf::CircleShape constraint_background{constraint.z};
+        constraint_background.setOrigin(constraint.z, constraint.z);
+        constraint_background.setFillerColor(sf::Color::Black);
+        constraint_background.setPosition(constraint.x, constraint.y); 
+        constraint_background.setPointCount(128);
+        m_target.draw(constraint_background);
+
+        //Rendering 
+        sf::CircleShape circle(1.0f);
+        circle.setPointCount(32);
+        circle.setOrigin(1.0f, 1.0f); 
+        const auto& objects = solver.getObjects();
+        for(const auto& obj : objects){
+          circle.setPosition(obj.position);
+          circle.setScale(obj.radius, obj.radius);
+          circle.setFillColor(obj.color);
+          m_target.draw(circle);
+        }
+      }
+  private:
+    sf::RenderTarget& m_target;
+};
