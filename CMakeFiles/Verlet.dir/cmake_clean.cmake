3f195f622f7d2b098a966e766d66a4d9652e27c0
Trible69 <edwinamunoz357@gmail.com>
2024-06-28 15:31:24 -0500

    Cmake 2

---
+++ CMakeFiles/Verlet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Verlet.dir/src/main.cpp.obj"
+  "CMakeFiles/Verlet.dir/src/main.cpp.obj.d"
+  "Verlet.exe"
+  "Verlet.exe.manifest"
+  "Verlet.pdb"
+  "libVerlet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Verlet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
